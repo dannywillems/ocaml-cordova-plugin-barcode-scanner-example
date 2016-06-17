1d225c59939f911ac2b7cbb405b9201a7eca5241
Danny Willems <contact@danny-willems.be>
2016-06-17 11:52:03 +0200

    Use functions instead of objects

--- app/ml/test.ml
+++ app/ml/test.ml
@@ -1,23 +1,11 @@
 module B = Cordova_barcode_scanner
 
 let on_device_ready _ =
-  let barcode = B.t () in
-  barcode#scan
-    (
-      fun res ->  Dom_html.window##(alert (Js.string (res#text ^ "\n" ^
-                  res#format)))
-    )
-    ()
-
-  (* Crash on a Nexus 5, android 6.0.1 *)
-  (*
-  barcode##(encode_err
-    (B.encode_format_to_str B.encode_text)
-    (Js.string "https://danny-willems.be")
-    (fun str -> Dom_html.window##(alert str))
-    (fun err -> Dom_html.window##(alert err))
-  );
-  *)
+  let print_res res =
+    Jsoo_lib.alert ((B.result_text res) ^ "\n" ^ (B.result_format res))
+  in
+  B.scan print_res ();
+  ()
 
 let _ =
   Cordova.Event.device_ready on_device_ready
